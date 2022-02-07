ec8e88aa96f759aa8295626a781675e81e94e9ae
brunobigaton <bfcavalcante@gmail.com>
2022-02-07 11:41:20 -0300

    Create MotivationConstants.kt

---
+++ motivation/app/src/main/java/com/example/motivation01/infra/MotivationConstants.kt
@@ -0,0 +1,12 @@
+package com.example.motivation01.infra
+
+class MotivationConstants private constructor(){
+    object KEY {
+        const val PERSON_NAME = "name"
+    }
+    object PHRASEFILTER {
+        const val ALL = 1
+        const val HAPPY = 2
+        const val MORNING = 3
+    }
+}
